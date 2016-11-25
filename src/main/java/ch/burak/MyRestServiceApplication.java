66b34a076dd331cb7c250ad14c475bbfbf4a6c18
KaraB <kara.b@windowslive.com>
2016-11-25 20:42:13 +0100

    Init first spring boot app

---
+++ src/main/java/ch/burak/MyRestServiceApplication.java
@@ -0,0 +1,12 @@
+package ch.burak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRestServiceApplication.class, args);
+	}
+}
