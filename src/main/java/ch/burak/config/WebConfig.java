cbe239c2cd3f398cb90602620c12977c926cec9b
KaraB <kara.b@windowslive.com>
2017-01-03 12:54:00 +0100

    Root redirects to /index closes #12

---
+++ src/main/java/ch/burak/config/WebConfig.java
@@ -0,0 +1,20 @@
+package ch.burak.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author Burak Kara
+ */
+@Configuration
+@ComponentScan
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:/index.html");
+    }
+
+}
